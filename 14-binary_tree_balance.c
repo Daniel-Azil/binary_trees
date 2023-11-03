f9a10e8a2d32fdbaee3492ca4cb17fa785b30d9c
Daniel Azil <danielazil.fse@gmail.com>
2023-11-03 00:53:59 +0000

    added file to given tasks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,24 +8,24 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (p_cfunction1(tree->left) - p_cfunction1(tree->right);
 
 	return (0);
 }
 
 /**
- * binary_tree_height - Custom function.
- * @tree: Argument of the custom function.
+ * p_cfunction1 - Custom function.
+ * @arg: Argument of the custom function.
  * Return: returns 0, ftn & vtn
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t p_cfunction1(const binary_tree_t *arg)
 {
-	if (tree)
+	if (arg)
 	{
 		size_t vtn = 0, ftn = 0;
 
-		vtn = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		ftn = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		vtn = arg->left ? 1 + p_cfunction1(arg->left) : 1;
+		ftn = arg->right ? 1 + p_cfunction1(arg->right) : 1;
 		return ((vtn > ftn) ? vtn : ftn);
 	}
 	return (0);
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * p_cfunc - Custom function
+ * @arg: Argument of custom function.
+ * Return: returns 0 or 1.
+ */
+int p_cfunc(const binary_tree_t *arg)
+{
+	if (arg != NULL)
+	{
+		if ((arg->left != NULL && arg->right == NULL) ||
+		    (arg->left == NULL && arg->right != NULL) ||
+		    p_cfunc(arg->left) == 0 ||
+		    p_cfunc(arg->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - Custom function
+ * @arg: Argument of custom function.
+ * Return:  returns 0 or 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (p_cfunc(tree));
+}
